c61ddaec11776ee39e0d430e63f2743d556c9c07
ミドリ <midori01@users.noreply.github.com>
2023-04-13 10:11:09 +0800

    Update Footer.tsx

--- web/hotaru-admin/src/components/Footer.tsx
+++ web/hotaru-admin/src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: FC = () => (
   <p className="text-center">
     Powered by
     {' '}
-    <a href="https://github.com/cokemine/nodestatus">NodeStatus</a>
+    <a href="https://github.com/midori01/nodestatus">NodeStatus</a>
   </p>
 );
 
