e61dec278203faa0d4ed515caee19b42ce782da6
ミドリ <midori01@users.noreply.github.com>
2023-04-23 05:04:25 +0800

    Update Management.tsx

--- web/hotaru-admin/src/pages/Management.tsx
+++ web/hotaru-admin/src/pages/Management.tsx
@@ -353,7 +353,7 @@ const Management: FC = () => {
                       <Input />
                     </Form.Item>
                     <Form.Item label="Password" name="password">
-                      <Input.Password placeholder="留空不修改" />
+                      <Input />
                     </Form.Item>
                     <Form.Item label="Name" name="name">
                       <Input />
