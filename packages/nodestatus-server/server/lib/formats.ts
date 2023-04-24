c8408bbc30a4c2a82cdc7de3a565428a41f3c174
ミドリ <midori01@users.noreply.github.com>
2023-04-25 01:10:52 +0900

    Delete formats.ts

--- packages/nodestatus-server/server/lib/formats.ts
+++
@@ -1,7 +0,0 @@
-export function formatNetwork(data: number): string {
-  if (data < 1024) return `${data.toFixed(0)}B`;
-  if (data < 1024 * 1024) return `${(data / 1024).toFixed(0)}K`;
-  if (data < 1024 * 1024 * 1024) return `${(data / 1024 / 1024).toFixed(1)}M`;
-  if (data < 1024 * 1024 * 1024 * 1024) return `${(data / 1024 / 1024 / 1024).toFixed(2)}G`;
-  return `${(data / 1024 / 1024 / 1024 / 1024).toFixed(2)}T`;
-}
