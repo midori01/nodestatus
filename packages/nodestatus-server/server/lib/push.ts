028d1111ed9bea465d3a2c3a267b820c400f008a
ミドリ <midori01@users.noreply.github.com>
2023-04-24 19:46:44 +0900

    Update push.ts

--- packages/nodestatus-server/server/lib/push.ts
+++ packages/nodestatus-server/server/lib/push.ts
@@ -76,7 +76,7 @@ export default function createPush(this: NodeStatus, options: PushOptions) {
       str += `CPU: ${Math.round(item.status.cpu)}% \n`;
       str += `内存: ${Math.round((item.status.memory_used / item.status.memory_total) * 100)}% \n`;
       str += `硬盘: ${Math.round((item.status.hdd_used / item.status.hdd_total) * 100)}% \n`;
-      str += `流量: ${Math.round(item.status.traffic)} \n`;
+      str += `流量: ↓${parseEntities((Number(item.status.network_in) / 1073741824).toFixed(2).toString())}G ↑${parseEntities((Number(item.status.network_out) / 1073741824).toFixed(2).toString())}G \n`;
       str += `在线: ${parseUptime(item.status.uptime)} \n`;
       str += '\n';
     });
