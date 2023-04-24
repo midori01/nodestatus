ad5380ce488d7cb65f3ce1bd326a0056dac0d462
ミドリ <midori01@users.noreply.github.com>
2023-04-25 01:06:01 +0900

    Update push.ts

--- packages/nodestatus-server/server/lib/push.ts
+++ packages/nodestatus-server/server/lib/push.ts
@@ -3,7 +3,7 @@ import { Telegraf } from 'telegraf';
 import HttpsProxyAgent from 'https-proxy-agent';
 import { logger } from './utils';
 import type NodeStatus from './nodestatus';
-import { formatNetwork } from './formats';
+import { computed } from 'vue';
 
 type PushOptions = {
   pushTimeOut: number;
@@ -24,6 +24,14 @@ const parseUptime = (uptime: number): string => {
   const s = String(Math.floor(uptime % 60)).padStart(2, '0');
   return `${h}:${m}:${s}`;
 };
+
+function formatNetwork(data: number): string {
+  if (data < 1024) return `${data.toFixed(0)}B`;
+  if (data < 1024 * 1024) return `${(data / 1024).toFixed(0)}K`;
+  if (data < 1024 * 1024 * 1024) return `${(data / 1024 / 1024).toFixed(1)}M`;
+  if (data < 1024 * 1024 * 1024 * 1024) return `${(data / 1024 / 1024 / 1024).toFixed(2)}G`;
+  return `${(data / 1024 / 1024 / 1024 / 1024).toFixed(2)}T`;
+}；
 
 export default function createPush(this: NodeStatus, options: PushOptions) {
   const pushList: Array<(message: string) => void> = [];
