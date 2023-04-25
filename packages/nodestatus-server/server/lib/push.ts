97301ba1d794f37782ac7cf9ff0ea9ddf7fae536
ミドリ <midori01@users.noreply.github.com>
2023-04-25 15:31:58 +0900

    Update push.ts

--- packages/nodestatus-server/server/lib/push.ts
+++ packages/nodestatus-server/server/lib/push.ts
@@ -24,6 +24,16 @@ const parseUptime = (uptime: number): string => {
   return `${h}:${m}:${s}`;
 };
 
+function formatByteSize(byteSize) {
+  const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+  let index = 0;
+  while (byteSize >= 1024 && index < units.length - 1) {
+    byteSize /= 1024;
+    index++;
+  }
+  return `${byteSize.toFixed(2)} ${units[index]}`;
+};
+
 export default function createPush(this: NodeStatus, options: PushOptions) {
   const pushList: Array<(message: string) => void> = [];
   /* Username -> timer */
@@ -76,7 +86,7 @@ export default function createPush(this: NodeStatus, options: PushOptions) {
       str += `CPU: ${Math.round(item.status.cpu)}% \n`;
       str += `内存: ${Math.round((item.status.memory_used / item.status.memory_total) * 100)}% \n`;
       str += `硬盘: ${Math.round((item.status.hdd_used / item.status.hdd_total) * 100)}% \n`;
-      str += `流量: ↓${parseEntities((Number(item.status.network_in) / 1073741824).toFixed(2).toString())}G ↑${parseEntities((Number(item.status.network_out) / 1073741824).toFixed(2).toString())}G \n`;
+      str += `流量: ↓${formatByteSize(item.status.network_in)} ↑${formatByteSize(item.status.network_out)} \n`;
       str += `在线: ${parseUptime(item.status.uptime)} \n`;
       str += '\n';
     });
