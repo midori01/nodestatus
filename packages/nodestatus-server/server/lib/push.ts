3ee32348456e29610de7b0cd0832ff1b2e169f63
ミドリ <midori01@users.noreply.github.com>
2023-04-25 08:28:29 +0800

    Update push.ts

--- packages/nodestatus-server/server/lib/push.ts
+++ packages/nodestatus-server/server/lib/push.ts
@@ -3,7 +3,6 @@ import { Telegraf } from 'telegraf';
 import HttpsProxyAgent from 'https-proxy-agent';
 import { logger } from './utils';
 import type NodeStatus from './nodestatus';
-import { computed } from 'vue';
 
 type PushOptions = {
   pushTimeOut: number;
@@ -24,16 +23,6 @@ const parseUptime = (uptime: number): string => {
   const s = String(Math.floor(uptime % 60)).padStart(2, '0');
   return `${h}:${m}:${s}`;
 };
-
-const formatNetwork = computed(() => {
-  return (data: number): string => {
-    if (data < 1024) return `${data.toFixed(0)}B`;
-    if (data < 1024 * 1024) return `${(data / 1024).toFixed(0)}K`;
-    if (data < 1024 * 1024 * 1024) return `${(data / 1024 / 1024).toFixed(1)}M`;
-    if (data < 1024 * 1024 * 1024 * 1024) return `${(data / 1024 / 1024 / 1024).toFixed(2)}G`;
-    return `${(data / 1024 / 1024 / 1024 / 1024).toFixed(2)}T`;
-  };
-});
 
 export default function createPush(this: NodeStatus, options: PushOptions) {
   const pushList: Array<(message: string) => void> = [];
@@ -87,7 +76,7 @@ export default function createPush(this: NodeStatus, options: PushOptions) {
       str += `CPU: ${Math.round(item.status.cpu)}% \n`;
       str += `内存: ${Math.round((item.status.memory_used / item.status.memory_total) * 100)}% \n`;
       str += `硬盘: ${Math.round((item.status.hdd_used / item.status.hdd_total) * 100)}% \n`;
-      str += `流量: ↓${formatNetwork(item.status.network_in)} ↑${formatNetwork(item.status.network_out)} \n`;
+      str += `流量: ↓${parseEntities((Number(item.status.network_in) / 1073741824).toFixed(2).toString())}G ↑${parseEntities((Number(item.status.network_out) / 1073741824).toFixed(2).toString())}G \n`;
       str += `在线: ${parseUptime(item.status.uptime)} \n`;
       str += '\n';
     });
