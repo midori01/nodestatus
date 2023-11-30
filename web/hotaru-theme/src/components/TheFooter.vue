1bec32e613ce0e9d2e70998b3a18b2fe1b5fd9b8
ミドリ <midori01@users.noreply.github.com>
2023-11-30 13:18:01 +0800

    Update TheFooter.vue

--- web/hotaru-theme/src/components/TheFooter.vue
+++ web/hotaru-theme/src/components/TheFooter.vue
@@ -12,8 +12,6 @@ export default defineComponent({
 });
 </script>
 
-<script async src="https://lvlv.lv/umami/script.js" data-website-id="8c6f1f0c-d6b3-44d0-b4f5-badfb366a181"></script>
-
 <style>
 .footer p {
   text-align: center;
